3de501d56da45fb0c789333f089cad3e1a43e631
Frank Wunderlich <frank-w@public-files.de>
2026-06-05 17:10:25 +0200

    mediatek: add 4e overlays only for 4e variant

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -753,8 +753,7 @@ define Device/bananapi_bpi-r4-pro-common
   DEVICE_DTS_LOADADDR := 0x45f00000
   DEVICE_DTS_OVERLAY:= mt7988a-bananapi-bpi-r4-pro-emmc mt7988a-bananapi-bpi-r4-pro-sd \
 		       mt7988a-bananapi-bpi-r4-pro-cn13 mt7988a-bananapi-bpi-r4-pro-cn14 \
-		       mt7988a-bananapi-bpi-r4-pro-cn15 mt7988a-bananapi-bpi-r4-pro-cn18 \
-		       mt7988a-bananapi-bpi-r4-pro-4e-wan-2g5 mt7988a-bananapi-bpi-r4-pro-4e-wan-sfp
+		       mt7988a-bananapi-bpi-r4-pro-cn15 mt7988a-bananapi-bpi-r4-pro-cn18
   DEVICE_DTC_FLAGS := --pad 4096
   DEVICE_PACKAGES := kmod-dsa-mxl862xx kmod-hwmon-pwmfan kmod-i2c-mux-pca954x \
 		     kmod-eeprom-at24 kmod-mt7996-firmware kmod-mt7996-233-firmware \
@@ -799,6 +798,7 @@ define Device/bananapi_bpi-r4-pro-4e
   DEVICE_DTS_CONFIG := config-mt7988a-bananapi-bpi-r4-pro-4e
   DEVICE_BL2 := comb
   $(call Device/bananapi_bpi-r4-pro-common)
+  DEVICE_DTS_OVERLAY += mt7988a-bananapi-bpi-r4-pro-4e-wan-2g5 mt7988a-bananapi-bpi-r4-pro-4e-wan-sfp
 endef
 TARGET_DEVICES += bananapi_bpi-r4-pro-4e
 
